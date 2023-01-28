f8385a3b2090e6cdb6645ef8d150c2babed4d77f
David Nuescheler <david@nuescheler.ch>
2023-01-28 12:44:47 -0700

    chore: more styling

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,7 +1,19 @@
 export default function decorate(block) {
-  const cell = block.querySelector('p').closest('div');
-  const bg = cell.querySelector('picture');
-  bg.closest('p').remove();
-  block.append(bg);
-  cell.classList.add('banner-body');
+  const row = block.firstElementChild;
+  const cols = [...row.children];
+  block.classList.add(`banner-${cols.length}-cols`);
+
+  if (cols.length === 2) {
+    const bg = row.querySelector('picture');
+    const bgDiv = bg.closest('div');
+    block.append(bg);
+    bgDiv.remove();
+    row.classList.add('banner-body');
+  }
+
+  if (cols.length === 3) {
+    ['image', 'body', 'cta'].forEach((e, i) => {
+      cols[i].classList.add(`banner-${e}`);
+    });
+  }
 }
